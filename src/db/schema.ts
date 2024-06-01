0a0c0ae3ad2d0cc6acbf1b60b85474ac5b74160a
Prathmesh Vhatkar <vhatkarps20.comp@coep.ac.in>
2024-06-01 18:14:10 +0530

    Added code to insertUser

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,21 +1,14 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import { integer, pgEnum, pgTable, serial, text, timestamp } from 'drizzle-orm/pg-core';
+import { integer, pgTable, serial, text, timestamp } from 'drizzle-orm/pg-core';
 
-
-//Enum for linkPrecedence
-export const LinkPrecedence = pgEnum('linkPrecedence', ['primary', 'secondary']);
 //Contact table
 export const Contact : any = pgTable('contact', {
     id: serial('id').primaryKey().notNull(),
     phoneNumber: text('phone_number'),
     email: text('email'),
     linkedId: integer('linked_id').references(() => Contact.id, { onDelete: 'cascade' }),
-    linkPrecedence: LinkPrecedence("linkPrecedence").default('primary').notNull(),
+    linkPrecedence: text("link_precedence").$type<"primary"| "secondary">(),
     createdAt: timestamp('created_at').notNull().defaultNow(),
     updatedAt: timestamp('updated_at').notNull().$onUpdate(() => new Date()),
     deletedAt: timestamp('deleted_at'),
 });
-
-// Define types for insert and select operations
-export type InsertContact = typeof Contact.$inferInsert;
-export type SelectContact = typeof Contact.$inferSelect;
--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,73 @@
+import e from "express";
 import { db } from "./db/db"
 import { Contact } from "./db/schema";
+import { or, eq } from 'drizzle-orm';
+
 
 const express = require('express')
 const app = express()
+app.use(express.json());
+
 const port = 3000
 
-app.get('/identify', (req: any, res: { send: (arg0: string) => void; }) => {
+app.get('/', (req, res) => {
     res.send('Hello World!')
 })
+
+app.get('/identify', (req, res) => {
+    res.send('POST request in the format at /identify')
+})
+
+async function checkIfContactExists(email, phoneNumber) {
+    try {
+        const result = await db.select().from(Contact).where(
+            or(
+                eq(Contact.email, email),
+                eq(Contact.phoneNumber, phoneNumber)
+            ));
+        return result;
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+async function InsertContact(email, phoneNumber) {
+    const existingUser = await checkIfContactExists(email, phoneNumber);
+    try { 
+        //set link Precedence
+        if (!Array.isArray(existingUser) || existingUser.length === 0) {
+            var precedenceVal = "primary"    
+        } else { 
+            precedenceVal = "secondary"
+        }
+
+        const user = db.insert(Contact).values({
+            email: email,
+            phoneNumber: phoneNumber,
+            linkPrecedence: precedenceVal,
+        }).returning({
+            id: Contact.id,
+        });
+        return user;
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+app.post('/identify', async (req, res) => {
+    try {
+        // Extract data from request body
+        const { email, phoneNumber } = req.body;
+
+        // InsertContact(email, phoneNumber);
+        const user = await InsertContact(email, phoneNumber);
+        console.log(user)
+
+    } catch (error: any) {
+        res.status(500).send(error.message);
+    }
+});
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
 })
-
-export default async function InsertContact() {
-    type NewContact = typeof Contact.$inferInsert;
-
-    const InsertContact = async (user: NewContact) => {
-        return db.insert(Contact).values(user);
-    }
-
-    const newContact: NewContact = { phoneNumber: "1234567890", email: "John@gmail.com", linkedId: null, linkPrecedence: "primary" };
-    await InsertContact(newContact);
-}
